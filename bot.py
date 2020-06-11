4ba3d24413616a008f50e0a1d1ff25e37137cf48
SF12-YT <62644015+SF12-YT@users.noreply.github.com>
2020-06-11 20:01:32 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ import random
 images = []
 videos = []
 quote = []
+songs = ["Winter Wonderland", "Kiss & Tell", "I Won't Apologize", "Falling Down", "I Promise You", "Crush", "Naturally", "The Way I Loved You", "More", "As a Blonde", "I Don't Miss You at All", "Stop & Erase", "I Got U", "Tell Me Something I Don't Know", "New Classic", "Magic", "Send It On", "Whoa Oh! (Me vs. Everyone) (Remix)", "One and the Same", "Round & Round", "A Year Without Rain", "Rock God", "Off the Chain", "Summer's Not Hot", "Intuition", "Spotlight", "Ghost of You", "Sick of You", "Live Like There's No Tomorrow", "Un Año Sin Lluvia", "Who Says", "Dices", "Love You Like a Love Song", "Bang Bang Bang", "We Own the Night", "Hit the Lights", "Whiplash", "When the Sun Goes Down", "My Dilemma", "That's More Like It", "Outlaw", "Middle of Nowhere", "Fantasma de Amor", "Shake It Up", "Come & Get It", "Slow Down", "Birthday", "Stars Dance", "Like a Champion", "Forget Forever", "Save the Day", "B.E.A.T.", "Write Your Name", "Undercover", "Love Will Remember", "Nobody Does It Like You", "Music Feels Better", "Lover in Me", "I Like It That Way", "The Heart Wants What It Wants", "Bidi Bidi Bom Bom", "Do It", "I Want You to Know", "Good for You", "Same Old Love", "Revival", "Kill Em with Kindness", "We Don't Talk Anymore", "Hands", "Trust Nobody", "Hands to Myself", "Sober", "Camouflage", "Me & the Rhythm", "Survivors", "Body Heat", "Rise", "Me & My Girls", "Nobody", "Perfect", "Outta My Hands (Loco)", "Cologne", "It Ain't Me", "Only You", "Bad Liar", "Fetish", "Wolves", "Back to You", "Taki Taki", "I Can't Get Enough", "Anxiety", "Lose You to Love Me", "Look at Her Now", "Rare", "Dance Again", "Ring", "Vulnerable", "People You Know", "Let Me Get Me", "Crowded Room", "Kinda Crazy", "Fun", "Cut You Off", "A Sweeter Place", "Feel Me"]
+albums = ["Kiss & Tell", "A Year Without Rain", "When the Sun Goes Down", "Stars Dance", "Revival", "Rare"]
 
 URLImg = ["https://images.search.yahoo.com/search/images;_ylt=AwrxhdeaAXteHUMAu4IN5gt.?p=selena+gomez+beautiful&ei=UTF-8&fr=yfp-t&fr2=p%3As%2Cv%3Ai", "https://images.search.yahoo.com/search/images;_ylt=AwrwJRipAXteg1kAcdYN5gt.;_ylu=X3oDMTBsZ29xY3ZzBHNlYwNzZWFyY2gEc2xrA2J1dHRvbg--;_ylc=X1MDMjExNDcwOTAwNQRfcgMyBGFjdG4DY2xrBGNzcmNwdmlkA2pjUkozREV3TGpKQ1J5aXpYbnJ4TGdRY05UZ3VNUUFBQUFCdUdGR1cEZnIDeWZwLXQEZnIyA3NhLWdwBGdwcmlkA1ZxbmQxN2xEUlRxUVA2Y3U3NXoxTUEEbl9zdWdnAzAEb3JpZ2luA2F1LmltYWdlcy5zZWFyY2gueWFob28uY29tBHBvcwMwBHBxc3RyAwRwcXN0cmwDBHFzdHJsAzIxBHF1ZXJ5A3NlbGVuYSUyMGdvbWV6JTIwY3V0ZQR0X3N0bXADMTU4NTExOTY4OQ--?p=selena+gomez+cute&fr=yfp-t&fr2=sb-top-au.images.search&ei=UTF-8&n=60&x=wrt", "https://images.search.yahoo.com/search/images;_ylt=AwrwJQ3JAXteP1AAwlEN5gt.;_ylu=X3oDMTBsZ29xY3ZzBHNlYwNzZWFyY2gEc2xrA2J1dHRvbg--;_ylc=X1MDMjExNDcwOTAwNQRfcgMyBGFjdG4DY2xrBGNzcmNwdmlkA0V2a1lMakV3TGpKQ1J5aXpYbnJ4TGdYcE5UZ3VNUUFBQUFCdl9ia2YEZnIDeWZwLXQEZnIyA3NhLWdwBGdwcmlkA3ZUYkhxc3ViU3RDeFNPQ3B0QmhqekEEbl9zdWdnAzAEb3JpZ2luA2F1LmltYWdlcy5zZWFyY2gueWFob28uY29tBHBvcwMwBHBxc3RyAwRwcXN0cmwDBHFzdHJsAzI1BHF1ZXJ5A3NlbGVuYSUyMGdvbWV6JTIwYWRvcmFibGUEdF9zdG1wAzE1ODUxMTk3MjM-?p=selena+gomez+adorable&fr=yfp-t&fr2=sb-top-au.images.search&ei=UTF-8&n=60&x=wrt"]
 URLYT = "https://www.youtube.com/user/SelGomez/videos?view=64&flow=grid"
@@ -36,15 +38,15 @@ for result in resultsQ():
         quote.append(div.text)
      except  AttributeError:
         pass
-print(quote)
 
-TOKEN = 'NjkxOTg3OTUwNDA1MTU2ODg3.XnrMMQ.p9lslKk-CbUyPSuxr1-AAQDluxc'
+TOKEN = 'NjkxOTg3OTUwNDA1MTU2ODg3.XpVK_Q.SfTGibBoPr61O0sTIv4ncG8heYU'
 
 client = discord.Client()
-embed = discord.Embed()
+emb = discord.Embed()
 
 @client.event
 async def on_ready():
+    await bot.change_presence(game=discord.Game(name="!SG help", type=2))
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
@@ -52,17 +54,31 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!SG image'):
+    if message.content.startswith('!SG pic'):
         try:
             ranimg = random.choice(images)
-            img = embed.set_image(url=ranimg)
+            img = emb.set_image(url=ranimg)
             await message.channel.send(embed=img)
-        except HTTPException:
+        except discord.errors.HTTPException:
             ranimg = random.choice(images)
-            img = embed.set_image(url=ranimg)
+            img = emb.set_image(url=ranimg)
+            await message.channel.send(embed=img)
+            
+    elif message.content.startswith('!SG Pic'):
+        try:
+            ranimg = random.choice(images)
+            img = emb.set_image(url=ranimg)
+            await message.channel.send(embed=img)
+        except discord.errors.HTTPException:
+            ranimg = random.choice(images)
+            img = emb.set_image(url=ranimg)
             await message.channel.send(embed=img)
 
     elif message.content.startswith('!SG video'):
+        ranvid = random.choice(videos)
+        await message.channel.send(ranvid)
+        
+    elif message.content.startswith('!SG Video'):
         ranvid = random.choice(videos)
         await message.channel.send(ranvid)
 
@@ -70,5 +86,34 @@ async def on_message(message):
         ranquote = random.choice(quote)
         embed = discord.Embed(title="**Selena Gomez Quotes!**", description=ranquote, color=0xfc88ff)
         await message.channel.send(embed = embed)
+        
+    elif message.content.startswith('!SG Quote'):
+        ranquote = random.choice(quote)
+        embed = discord.Embed(title="**Selena Gomez Quotes!**", description=ranquote, color=0xfc88ff)
+        await message.channel.send(embed = embed)
 
+    elif message.content.startswith('!SG song'):
+        ransng = random.choice(songs)
+        await message.channel.send(ransng)
+        
+    elif message.content.startswith('!SG Song'):
+        ransng = random.choice(songs)
+        await message.channel.send(ransng)
+
+    elif message.content.startswith('!SG album'):
+        ranalb = random.choice(albums)
+        await message.channel.send(ranalb)
+        
+    elif message.content.startswith('!SG Album'):
+        ranalb = random.choice(albums)
+        await message.channel.send(ranalb)
+        
+    elif message.content.startswith('!SG help'):
+        embed = discord.Embed(title="**Commands Help!**", description="**Every command starts with an !SG** \n Use *video* to get a **Selena Gomez video** \n Use *quote* to get a **Selena Gomez quote** \n Use *song* to get a **Selena Gomez song** \n Use *album* to get a **Selena Gomez album**", color=0xfc88ff)
+        await message.channel.send(embed = embed)
+
+    elif message.content.startswith('!SG Help'):
+        embed = discord.Embed(title="**Commands Help!**", description="**Every command starts with an !SG** \n Use *video* to get a **Selena Gomez video** \n Use *quote* to get a **Selena Gomez quote** \n Use *song* to get a **Selena Gomez song** \n Use *album* to get a **Selena Gomez album**", color=0xfc88ff)
+        await message.channel.send(embed = embed)
+  
 client.run(TOKEN)
