42413dbd2a083f6b668f5143da78e79e2a2edb21
SF12-YT <62644015+SF12-YT@users.noreply.github.com>
2020-06-11 21:01:10 +1000

    Changed from 'game' to 'activity'

--- bot.py
+++ bot.py
@@ -47,7 +47,7 @@ emb = discord.Embed()
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.Game(name="!SG help", type=3))
+    await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.listening, name="!SG help"))
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
